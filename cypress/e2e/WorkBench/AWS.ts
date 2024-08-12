af44fe24820908cd5c4d54fe524c319076a6650f
prabhtra01 <prabhtra01@gmail.com>
2024-08-12 16:50:29 +0530

    Add AWS.ts file for WorkBench

---
+++ cypress/e2e/WorkBench/AWS.ts
@@ -0,0 +1,8 @@
+//Write a program to add two numbers in typescript 
+function addTwoNumbers(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+//how to call the above function 
+let sum: number = addTwoNumbers(10, 20);
+
